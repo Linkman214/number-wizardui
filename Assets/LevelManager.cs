1ccd70c72e96843ac48d2933cfde630928108971
Linkman214 <Linkman214@yahoo.com>
2016-06-05 14:29:35 -0400

    Add files via upload

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void Loadlevel(string name){
+
+	Debug.Log("Level load requested for:" + name);
+	SceneManager.LoadScene(name);
+
+	}
+
+
+	public void QuitGame() {
+
+	Debug.Log("Quit");
+	Application.Quit();
+
+	}
+
+	public void Reset() {
+	SceneManager.LoadScene(name);
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;  // highest number you can choose
+	int min;     // lowest number you can choose
+	int guess;
+	int maxGuesses=10;
+
+	public Text text; 
+	
+	void Start () {
+		
+	StarGame();
+		
+	
+	
+	}
+	void StarGame() {
+		max =1000;  // highest number you can choose
+		min= 1;     // lowest number you can choose
+		NextGuess();
+
+		}
+	
+	public void GuessHigher(){
+		min=guess;
+		NextGuess();
+	}
+
+	public void GuessLower(){
+		max=guess;
+		NextGuess();
+	}
+
+
+	void NextGuess() {
+		maxGuesses= maxGuesses-1;
+		guess= Random.Range(min,max+1);
+		text.text=guess.ToString();
+		if(maxGuesses<=0) {
+		SceneManager.LoadScene("Win");
+	}
+
+	}
+
+}
